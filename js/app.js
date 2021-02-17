68591f2f5b8575efefd80883d75c5cca6c0afc21
swapnilbhakare <swapnilbhakare7@gmail.com>
2021-02-17 21:44:12 +0530

    card add

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-window.addEventListener("resize", function() {
-    addRequireClass();
+window.addEventListener("resize", function () {
+  addRequireClass();
 });
 
 function addRequireClass() {
-    if (window.innerWidth < 860) {
-        document.body.classList.add("mobile");
-    } else {
-        document.body.classList.remove("mobile");
-    }
+  if (window.innerWidth < 860) {
+    document.body.classList.add("mobile");
+  } else {
+    document.body.classList.remove("mobile");
+  }
 }
 window.onload = addRequireClass;
 
@@ -17,46 +17,47 @@ const bars = document.querySelectorAll(".hamburger span");
 
 let isActive = false;
 
-hamburger.addEventListener("click", function() {
-    mobileNav.classList.toggle("open");
-    if (!isActive) {
-        bars[0].style.transform = "rotate(45deg)";
-        bars[1].style.opacity = "0";
-        bars[2].style.transform = "rotate(-45deg)";
-        isActive = true;
-    } else {
-        bars[0].style.transform = "rotate(0deg)";
-        bars[1].style.opacity = "1";
-        bars[2].style.transform = "rotate(0deg)";
-        isActive = false;
-    }
+hamburger.addEventListener("click", function () {
+  mobileNav.classList.toggle("open");
+  if (!isActive) {
+    bars[0].style.transform = "rotate(45deg)";
+    bars[1].style.opacity = "0";
+    bars[2].style.transform = "rotate(-45deg)";
+    isActive = true;
+  } else {
+    bars[0].style.transform = "rotate(0deg)";
+    bars[1].style.opacity = "1";
+    bars[2].style.transform = "rotate(0deg)";
+    isActive = false;
+  }
 });
 
-const school = [{
-        id: 1,
-        name: "sb high school aurangabad",
-        city: "aurangabad",
-        rating: 4.2,
-        img: "./images/sb-high.jpg",
-        desc: `Keeping the betterment of the students when it comes to academic, physical and mental progress paramount is S B High School in Aurangabad-maharashtra. `,
-    },
-    {
-        id: 1,
-        name: "Blue Ridge Public School",
-        city: "Pune",
-        rating: 4.2,
-        img: "./images/blue-ridge.jpeg",
-        desc: `BREI’s Blue Ridge Public School has been a rocking success from the very first year of its inception in the year 2010. Owing to the well-balanced approach, practised by the school, towards the holistic development of the students, schooling has been an enjoyable experience for the children and it is well-reflected in the overwhelming response that we got for the second academic year.`,
-    },
+const schools = [
+  {
+    id: 1,
+    name: "sb high school aurangabad",
+    city: "aurangabad",
+    rating: 4.2,
+    img: "./images/sb-high.jpg",
+    desc: `Keeping the betterment of the students when it comes to academic, physical and mental progress paramount is S B High School in Aurangabad-maharashtra. `,
+  },
+  {
+    id: 1,
+    name: "Blue Ridge Public School",
+    city: "Pune",
+    rating: 4.2,
+    img: "./images/blue-ridge.jpeg",
+    desc: `BREI’s Blue Ridge Public School has been a rocking success from the very first year of its inception in the year 2010. Owing to the well-balanced approach, practised by the school, towards the holistic development of the students, schooling has been an enjoyable experience for the children and it is well-reflected in the overwhelming response that we got for the second academic year.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
 
-window.addEventListener("DOMContentLoaded", function() {
-    let displayschool = schools.map(function(school) {
-        // console.log(item);
+window.addEventListener("DOMContentLoaded", function () {
+  let displayschools = schools.map(function (school) {
+    // console.log(item);
 
-        return `  <div class="card">
+    return `  <div class="card ">
     <div class="card-image">
       <img src=${school.img} alt=${school.name} />
     </div>
@@ -69,9 +70,9 @@ window.addEventListener("DOMContentLoaded", function() {
     </div>
   </div>
 `;
-    });
-    displayschool = displayschool.join("");
-    console.log(displayschool);
+  });
+  displayschools = displayschools.join("");
+  console.log(displayschools);
 
-    sectionCenter.innerHTML = displayschool;
+  sectionCenter.innerHTML = displayschools;
 });
